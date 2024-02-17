804c5ed1bd26c38279e065fdf0894bcd603e7b23
Carlos Erick Rosario <carloserickrosario@gmail.com>
2024-02-17 15:11:09 -0300

    Pages frontend with routes url React app

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+//Router para criar rotas, Routes definir as rotas e Route a rota!
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+function App() {
+  return (
+    <>
+      <Router> 
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Dashboard/>}></Route>
+            <Route path='/login' element={<Login/>}></Route>
+            <Route path='/register' element={<Register/>}></Route>
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
